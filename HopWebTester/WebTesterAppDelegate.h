d1bfa79aa01a8f748800bbbfac30c3ceee6c0967
celer <celer@coyote.(none)>
2013-01-24 20:34:20 -0700

    Initial Commit

---
+++ HopWebTester/WebTesterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WebTesterAppDelegate.h
+//  HopWebTester
+//
+//  Created by celer on 1/24/13.
+//  Copyright (c) 2013 hopjs.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WebTesterAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ HopWebTester/WebTesterAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  WebTesterAppDelegate.m
+//  HopWebTester
+//
+//  Created by celer on 1/24/13.
+//  Copyright (c) 2013 hopjs.org. All rights reserved.
+//
+
+#import "WebTesterAppDelegate.h"
+
+@implementation WebTesterAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
